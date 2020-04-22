a5120a03b4d405a0ba0455983834073ff244d53f
s17025 <61729906+s17025@users.noreply.github.com>
2020-04-22 16:51:31 +0200

    Wykład

---
+++ AuthenticationSampleWebApp/AuthenticationSampleWebApp/Handlers/BasicAuthHandlers.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace AuthenticationSampleWebApp.Handlers
+{
+    public class BasicAuthHandlers : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public BasicAuthHandlers(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock) : base(options, logger, encoder, clock)
+            //IStudentDbService service) : base(options, logger, encoder, clock)
+        {
+
+        }
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+                return AuthenticateResult.Fail("Missing authorization haeder");
+
+            //"Autorization: Basic xxxxx"
+            var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+            var credentialBytes = Convert.FromBase64String(authHeader.Parameter); //xxxxx => bajty -> jan123:haslo123
+            var credentials = Encoding.UTF8.GetString(credentialBytes).Split(":");
+
+            if(credentials.Length != 2)
+                return AuthenticateResult.Fail("Incorrect authorization header value");
+
+            //TODO chceck credentials id DB
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+                new Claim(ClaimTypes.Name, "jan123"),
+                new Claim(ClaimTypes.Role, "admin"),
+                new Claim(ClaimTypes.Role, "student"),
+            };
+
+            var identity = new ClaimsIdentity(claims, Scheme.Name); //np Basic, ...
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+            return AuthenticateResult.Success(ticket);
+        }
+    }
+}
